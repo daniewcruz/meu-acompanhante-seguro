f97ebf5b38952cda9e9d6ca3d090b376d77b982d
Daniel Cruz <daniewcruz@gmail.com>
2024-06-20 12:00:39 -0300

    ajuste, no botão de audio e mudanças nas imagens

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -101,26 +101,17 @@ function fecharDialog3(){
 
 
 
-/// leitor de texto por voz///
-
 let estaLendo = false;
 let partesTexto = []; // Array para armazenar os objetos SpeechSynthesisUtterance
 let indiceParteAtual = 0; // Índice da parte atual sendo lida
 let pausadoEmParte = null; // Armazena o utterance onde a leitura foi pausada
 
-// Seleciona o botão de leitura e a imagem
-const botaoLer = document.getElementById('BotaoAudio');
-const imgAudio = document.getElementsByClassName('imagemAudio');
-
-// Função para inicializar a leitura do texto completo
-function iniciarLeitura() {
+// Função para iniciar a leitura de um texto específico
+function iniciarLeitura(texto) {
     // Limpa o array de partesTexto apenas se não estiver vazio
     if (partesTexto.length > 0) {
         partesTexto = [];
     }
-
-    // Obtém o texto do parágrafo
-    const texto = document.getElementById('texto').textContent.trim();
 
     // Divide o texto em partes (por exemplo, por parágrafos)
     const partes = texto.split('\n').filter(part => part.trim() !== '');
@@ -132,7 +123,7 @@ function iniciarLeitura() {
 
         // Configurações adicionais do objeto parteTexto, se necessário
         parteTexto.voice = speechSynthesis.getVoices().find(voice => voice.lang === 'pt-BR');
-        
+
         // Evento para quando a leitura termina
         parteTexto.onend = () => {
             // Marca o índice da parte que foi lida completamente
@@ -144,7 +135,7 @@ function iniciarLeitura() {
                 // Se todas as partes foram lidas, redefine as variáveis
                 indiceParteAtual = 0;
                 estaLendo = false;
-                imgAudio.src = 'assets/images/icon_audio.png';
+                document.querySelectorAll('.imagemAudio').forEach(img => img.src = 'assets/images/icon_audio.png');
             }
         };
 
@@ -152,7 +143,7 @@ function iniciarLeitura() {
         parteTexto.onerror = (event) => {
             console.error('Erro na síntese de fala:', event.error);
             estaLendo = false;
-            imgAudio.src = 'assets/images/icon_audio.png';
+            document.querySelectorAll('.imagemAudio').forEach(img => img.src = 'assets/images/icon_audio.png');
         };
     });
 
@@ -170,30 +161,40 @@ function iniciarProximaParte() {
         } else {
             speechSynthesis.speak(parteTexto);
         }
-        imgAudio.src = 'assets/images/icon_audio_stop.png'; // Imagem de parar leitura
+        document.querySelectorAll('.imagemAudio').forEach(img => img.src = 'assets/images/icon_pausa.png');
         estaLendo = true;
     }
 }
 
-// Evento de clique no botão de leitura
-botaoLer.addEventListener('click', () => {
-    if (estaLendo) {
-        // Pausa a leitura se já estiver lendo
-        speechSynthesis.pause();
-        pausadoEmParte = partesTexto[indiceParteAtual]; // Armazena o parteTexto onde a leitura foi pausada
-        imgAudio.src = 'assets/images/icon_audio.png';
-        estaLendo = false;
-    } else {
-        // Se a leitura estiver pausada, retoma a partir da parte atual
-        if (indiceParteAtual < partesTexto.length) {
-            speechSynthesis.resume();
-            imgAudio.src = 'assets/images/icon_audio_stop.png'; // Imagem de parar leitura
-            estaLendo = true;
+// Função para tratar o clique no botão de leitura de um dialog específico
+function configurarBotaoLeitura(botaoLer, dialogo) {
+    const imgAudio = botaoLer.querySelector('.imagemAudio');
+    botaoLer.addEventListener('click', () => {
+        const textoDialogo = dialogo.querySelector('.textoDialogo').textContent.trim();
+        if (estaLendo) {
+            // Pausa a leitura se já estiver lendo
+            speechSynthesis.pause();
+            pausadoEmParte = partesTexto[indiceParteAtual]; // Armazena o parteTexto onde a leitura foi pausada
+            imgAudio.src = 'assets/images/icon_audio.png';
+            estaLendo = false;
         } else {
-            // Inicia a leitura completa se não estiver lendo nada
-            iniciarLeitura();
+            // Se a leitura estiver pausada, retoma a partir da parte atual
+            if (indiceParteAtual < partesTexto.length && pausadoEmParte) {
+                speechSynthesis.resume();
+                imgAudio.src = 'assets/images/icon_pausa.png';
+                estaLendo = true;
+            } else {
+                // Inicia a leitura do texto do dialogo específico
+                iniciarLeitura(textoDialogo);
+            }
         }
-    }
+    });
+}
+
+// Configurar todos os botões de leitura
+document.querySelectorAll('.BotaoAudio').forEach(botaoLer => {
+    const dialogo = botaoLer.closest('.dialogRisco');
+    configurarBotaoLeitura(botaoLer, dialogo);
 });
 
 // Evento para lidar com o encerramento da página ou aplicação
@@ -202,7 +203,95 @@ window.addEventListener('beforeunload', () => {
     speechSynthesis.cancel();
 });
 
-    
-  
 
-    
+
+
+// Função para inicializar a leitura do texto completo
+// function iniciarLeitura() {
+//     // Limpa o array de partesTexto apenas se não estiver vazio
+//     if (partesTexto.length > 0) {
+//         partesTexto = [];
+//     }
+
+//     // Obtém o texto do parágrafo
+//     const texto = document.getElementById('texto').textContent.trim();
+
+//     // Divide o texto em partes (por exemplo, por parágrafos)
+//     const partes = texto.split('\n').filter(part => part.trim() !== '');
+
+//     // Cria um objeto SpeechSynthesisUtterance para cada parte
+//     partes.forEach(part => {
+//         const parteTexto = new SpeechSynthesisUtterance(part);
+//         partesTexto.push(parteTexto);
+
+//         // Configurações adicionais do objeto parteTexto, se necessário
+//         parteTexto.voice = speechSynthesis.getVoices().find(voice => voice.lang === 'pt-BR');
+        
+//         // Evento para quando a leitura termina
+//         parteTexto.onend = () => {
+//             // Marca o índice da parte que foi lida completamente
+//             indiceParteAtual++;
+//             // Se ainda houver partes para ler, inicia a próxima parte
+//             if (indiceParteAtual < partesTexto.length) {
+//                 iniciarProximaParte();
+//             } else {
+//                 // Se todas as partes foram lidas, redefine as variáveis
+//                 indiceParteAtual = 0;
+//                 estaLendo = false;
+//                 imgAudio.src = 'assets/images/icon_audio.png';
+//             }
+//         };
+
+//         // Evento para lidar com erros na leitura
+//         parteTexto.onerror = (event) => {
+//             console.error('Erro na síntese de fala:', event.error);
+//             estaLendo = false;
+//             imgAudio.src = 'assets/images/icon_audio.png';
+//         };
+//     });
+
+//     // Inicia a primeira parte da leitura
+//     iniciarProximaParte();
+// }
+
+// // Função para iniciar a próxima parte da leitura
+// function iniciarProximaParte() {
+//     if (indiceParteAtual < partesTexto.length) {
+//         const parteTexto = partesTexto[indiceParteAtual];
+//         // Se foi pausado anteriormente, retoma a partir do parteTexto onde foi pausado
+//         if (pausadoEmParte === parteTexto) {
+//             speechSynthesis.resume();
+//         } else {
+//             speechSynthesis.speak(parteTexto);
+//         }
+//         imgAudio.src = 'assets/images/icon_pausa.png'; // Imagem de parar leitura
+//         estaLendo = true;
+//     }
+// }
+
+// // Evento de clique no botão de leitura
+// botaoLer.addEventListener('click', () => {
+//     if (estaLendo) {
+//         // Pausa a leitura se já estiver lendo
+//         speechSynthesis.pause();
+//         pausadoEmParte = partesTexto[indiceParteAtual]; // Armazena o parteTexto onde a leitura foi pausada
+//         imgAudio.src = 'assets/images/icon_audio.png';
+//         estaLendo = false;
+//     } else {
+//         // Se a leitura estiver pausada, retoma a partir da parte atual
+//         if (indiceParteAtual < partesTexto.length) {
+//             speechSynthesis.resume();
+//             imgAudio.src = 'assets/images/icon_pausa.png'; // Imagem de parar leitura
+//             estaLendo = true;
+//         } else {
+//             // Inicia a leitura completa se não estiver lendo nada
+//             iniciarLeitura();
+//         }
+//     }
+// });
+
+// // Evento para lidar com o encerramento da página ou aplicação
+// window.addEventListener('beforeunload', () => {
+//     // Cancela a leitura ao sair da página ou aplicação
+//     speechSynthesis.cancel();
+// });
